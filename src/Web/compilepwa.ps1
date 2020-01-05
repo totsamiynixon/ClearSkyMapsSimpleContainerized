9272c7fb3e0855d437f9d555d9da60ec74e232fe
totsamiynixon <totsamiynixon@gmail.com>
2020-01-06 02:58:27 +0300

    upd

--- src/Web/compilepwa.ps1
+++ src/Web/compilepwa.ps1
@@ -1,21 +1,19 @@
-[PSCustomObject]$configJSON = Get-Content -Path "./config.json" | ConvertFrom-Json;
 
-$version = "";
-if (!$configJSON."Application:Version") { 
-    $version = ([TimeSpan] (Get-Date).ToShortTimeString()).TotalDays + ([TimeSpan] (Get-Date).ToShortTimeString()).TotalMilliseconds;
-} 
-else { $version = $configJSON."Application:Version" }
+$rootPath = $env:PWA_ROOT_PATH;
+[PSCustomObject]$configJSON = Get-Content -Path ($rootPath + "config.json") | ConvertFrom-Json;
 
-$manifestJSON = Get-Content -Path "./Areas/PWA/manifest.json" | ConvertFrom-Json;
+$version = $configJSON."Application:Version";
+
+$manifestJSON = Get-Content -Path ($rootPath + "Areas/PWA/manifest.json") | ConvertFrom-Json;
 $manifestJSON."name" = $manifestJSON."name" + " " + $configJSON."Application:Environment";
 $manifestJSON."short_name" = $manifestJSON."short_name" + " " + $configJSON."Application:Environment";
 $manifestJSON | Add-Member -NotePropertyName gsm_sender_id -NotePropertyValue  $configJSON."FirebaseCloudMessaging:MessagingSenderId";
 
-$manifestJSON | ConvertTo-Json -depth 100 | Out-File "./wwwroot/pwa/manifest.json";
+$manifestJSON | ConvertTo-Json -depth 100 | Out-File ($rootPath  + "wwwroot/pwa/manifest.json");
 
 
-$serviceWorker =  Get-Content -Path "./Areas/PWA/service-worker.js";
+$serviceWorker =  Get-Content -Path ($rootPath  + "Areas/PWA/service-worker.js");
 $serviceWorker = $serviceWorker -replace '%MessagingSenderId%', $configJSON."FirebaseCloudMessaging:MessagingSenderId";
 $serviceWorker = $serviceWorker -replace '%Version%', $version;
 
-$serviceWorker | Set-Content -Path "./wwwroot/pwa/service-worker.js";
+$serviceWorker | Set-Content -Path ($rootPath + "wwwroot/pwa/service-worker.js");
